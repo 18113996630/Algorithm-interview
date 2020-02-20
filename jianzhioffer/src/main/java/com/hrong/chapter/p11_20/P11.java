9ac251cba9b273c869187b9394cf322b2ef46bb8
huangrong <1011486768@qq.com>
2020-02-20 17:45:19 +0800

    feat:p11

---
+++ jianzhioffer/src/main/java/com/hrong/chapter/p11_20/P11.java
@@ -0,0 +1,17 @@
+package com.hrong.chapter.p11_20;
+
+/**
+ * @Author hrong
+ * @Description 二进制中1的个数
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ **/
+public class P11 {
+	public static void main(String[] args) {
+	}
+
+
+
+	public int NumberOf1(int n) {
+		return 0;
+	}
+}
